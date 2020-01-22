58bef2981030fe46df189c38c33fbf16960d5a5a
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-22 18:44:31 -0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main() {
-    
+    string name = "Kevin";
+    int age = 30;
+    stringstream ss;
+    ss << "Name is: ";
+    ss << name;
+    ss << "; Age is: ";
+    ss << age;
+    string info = ss.str();
+    cout << info << endl;
     return 0;
 }
